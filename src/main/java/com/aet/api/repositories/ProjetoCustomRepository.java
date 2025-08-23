7acc31d37df84829a368f261aa19d389afa9e2c1
Diego Donizete Correa da Silva <ddonizetecorrea@gmail.com>
2025-08-23 09:32:19 -0400

    fix: ajuste no retorno dos dados

--- src/main/java/com/aet/api/repositories/ProjetoCustomRepository.java
+++ src/main/java/com/aet/api/repositories/ProjetoCustomRepository.java
@@ -94,133 +94,140 @@ public interface ProjetoCustomRepository extends JpaRepository<Projeto, Long> {
     List<Unidade2DTO> findRodizio(Long projetoId);
 
     @Query(value = """
-            SELECT p.projeto_id, sum(TipoEixo_Peso),
-            count(distinct tn.TVE_Nivel_Nivel) as Niveis,
-            sum(case when tn.TVE_Nivel_Nivel = 1 and tn1.TipoEixo_rodas > 0 then 1 else 0 end) as qtd_nivel_1,
-            sum(case when tn.TVE_Nivel_Nivel = 2 and tn1.TipoEixo_rodas > 0 then 1 else 0 end) as qtd_nivel_2,
-            sum(case when tn.TVE_Nivel_Nivel = 3 and tn1.TipoEixo_rodas > 0 then 1 else 0 end) as qtd_nivel_3,
-            sum(case when tn.TVE_Nivel_Nivel = 4 and tn1.TipoEixo_rodas > 0 then 1 else 0 end) as qtd_nivel_4,
-            mv.modelo_veiculo_reducao_1ic,
-            mv.modelo_veiculo_reducao_2ic,
-            mv.modelo_veiculo_reducao_3ic,
-            mv.modelo_veiculo_reducao_4ic,
-            mv.modelo_veiculo_reducao_5ic,
-            mv.modelo_veiculo_reducao_6ic,
-            mv.modelo_veiculo_reducao_7ic,
-            mv.modelo_veiculo_reducao_8ic,
-            mv.modelo_veiculo_reducao_9ic,
-            mv.modelo_veiculo_reducao_10ic,
-            mv.modelo_veiculo_reducao_11ic,
-            mv.modelo_veiculo_reducao_12ic,
-            mv.modelo_veiculo_reducao_13ic,
-            mv.modelo_veiculo_reducao_14ic,
-            mv.modelo_veiculo_reducao_15ic,
-            mv.modelo_veiculo_reducao_16ic,
-            mv.modelo_veiculo_reducao_17ic,
-            mv.modelo_veiculo_reducao_18ic,
-            mv.modelo_veiculo_reducao_19ic,
-            mv.modelo_veiculo_reducao_20ic,
-            case
-            when coalesce(mv.modelo_veiculo_reducao_1ic,0) = 0 then 0
-            when coalesce(mv.modelo_veiculo_reducao_2ic,0) = 0 then 1
-            when coalesce(mv.modelo_veiculo_reducao_3ic,0) = 0 then 2
-            when coalesce(mv.modelo_veiculo_reducao_4ic,0) = 0 then 3
-            when coalesce(mv.modelo_veiculo_reducao_5ic,0) = 0 then 4
-            when coalesce(mv.modelo_veiculo_reducao_6ic,0) = 0 then 5
-            when coalesce(mv.modelo_veiculo_reducao_7ic,0) = 0 then 6
-            when coalesce(mv.modelo_veiculo_reducao_8ic,0) = 0 then 7
-            when coalesce(mv.modelo_veiculo_reducao_9ic,0) = 0 then 8
-            when coalesce(mv.modelo_veiculo_reducao_10ic,0) = 0 then 9
-            when coalesce(mv.modelo_veiculo_reducao_11ic,0) = 0 then 10
-            when coalesce(mv.modelo_veiculo_reducao_12ic,0) = 0 then 11
-            when coalesce(mv.modelo_veiculo_reducao_13ic,0) = 0 then 12
-            when coalesce(mv.modelo_veiculo_reducao_14ic,0) = 0 then 13
-            when coalesce(mv.modelo_veiculo_reducao_15ic,0) = 0 then 14
-            when coalesce(mv.modelo_veiculo_reducao_16ic,0) = 0 then 15
-            when coalesce(mv.modelo_veiculo_reducao_17ic,0) = 0 then 16
-            when coalesce(mv.modelo_veiculo_reducao_18ic,0) = 0 then 17
-            when coalesce(mv.modelo_veiculo_reducao_19ic,0) = 0 then 18
-            when coalesce(mv.modelo_veiculo_reducao_20ic,0) = 0 then 19
-            else 0 end as qtd_marchaa,
-            mv.modelo_veiculo_coef_atrito as coeficiente_atrito,
-            mv.modelo_veiculo_raio_dinam as raio,
-            mv.modelo_veiculo_resist_rolam as resistencia_rolamento,
-            mv.modelo_veiculo_tipo_pneu as tipo_pneu,
-            mv.modelo_veiculo_torque_max as torque,
-            mv.modelo_veiculo_reducao_max as reducao_max
-            from aet04.projeto p
-            inner join aet04.ordem_servico_servico oss on oss.ordem_servico_servico_id = p.ordem_servico_servico_id
-            inner join aet04.veiculo v on oss.veiculo_id = v.veiculo_id
-            inner join aet04.modelo_veiculo mv on mv.modelo_veiculo_id = v.modelo_veiculo_id
-            inner join aet04.tipo_servico ts on ts.tipo_servico_id = oss.tipo_servico_id 
-            inner join aet04.tipo_veiculo tv on tv.tipo_veiculo_id  = ts.tipo_veiculo_id
-            inner join aet04.tipo_veiculo_eixo tve on tve.tipo_veiculo_id  = tv.tipo_veiculo_id
-            and tve.tipo_veiculoEixo_numeixos = p.projeto_num_eixos
-            and tve.modelo_veiculo_id = mv.modelo_veiculo_id
-            inner join aet04.tve_nivel tn on tn.tipo_veiculoEixo_id = tve.tipo_veiculoEixo_id
-            inner join aet04.tve_niveltipoeixo tn1 on tn1.TVE_Nivel_id = tn.TVE_Nivel_id
-            where p.projeto_id = :projetoId
-             and tv.tipo_veiculo_ativo = 1
-            and (select sum(a.TipoEixo_distancia ) from aet04.tve_nivel b
-            inner join aet04.tve_niveltipoeixo a on a.TVE_Nivel_id = b.TVE_Nivel_id
-            	where b.tipo_veiculoEixo_id = tve.tipo_veiculoEixo_id) = p.projeto_comprimento            
-            """, nativeQuery = true)
-    ProjetoResumoEixosDTO buscarResumoPorProjeto(Long projetoId);
+SELECT 
+    p.projeto_id                                      AS projetoId,
+    SUM(TipoEixo_Peso)                                AS qtdTipoeixoPeso,
+    COUNT(DISTINCT tn.TVE_Nivel_Nivel)                AS niveis,
+    SUM(CASE WHEN tn.TVE_Nivel_Nivel = 1 AND tn1.TipoEixo_rodas > 0 THEN 1 ELSE 0 END) AS qtdNivel1,
+    SUM(CASE WHEN tn.TVE_Nivel_Nivel = 2 AND tn1.TipoEixo_rodas > 0 THEN 1 ELSE 0 END) AS qtdNivel2,
+    SUM(CASE WHEN tn.TVE_Nivel_Nivel = 3 AND tn1.TipoEixo_rodas > 0 THEN 1 ELSE 0 END) AS qtdNivel3,
+    SUM(CASE WHEN tn.TVE_Nivel_Nivel = 4 AND tn1.TipoEixo_rodas > 0 THEN 1 ELSE 0 END) AS qtdNivel4,
+
+    mv.modelo_veiculo_reducao_1ic   AS modeloVeiculoReducao1ic,
+    mv.modelo_veiculo_reducao_2ic   AS modeloVeiculoReducao2ic,
+    mv.modelo_veiculo_reducao_3ic   AS modeloVeiculoReducao3ic,
+    mv.modelo_veiculo_reducao_4ic   AS modeloVeiculoReducao4ic,
+    mv.modelo_veiculo_reducao_5ic   AS modeloVeiculoReducao5ic,
+    mv.modelo_veiculo_reducao_6ic   AS modeloVeiculoReducao6ic,
+    mv.modelo_veiculo_reducao_7ic   AS modeloVeiculoReducao7ic,
+    mv.modelo_veiculo_reducao_8ic   AS modeloVeiculoReducao8ic,
+    mv.modelo_veiculo_reducao_9ic   AS modeloVeiculoReducao9ic,
+    mv.modelo_veiculo_reducao_10ic  AS modeloVeiculoReducao10ic,
+    mv.modelo_veiculo_reducao_11ic  AS modeloVeiculoReducao11ic,
+    mv.modelo_veiculo_reducao_12ic  AS modeloVeiculoReducao12ic,
+    mv.modelo_veiculo_reducao_13ic  AS modeloVeiculoReducao13ic,
+    mv.modelo_veiculo_reducao_14ic  AS modeloVeiculoReducao14ic,
+    mv.modelo_veiculo_reducao_15ic  AS modeloVeiculoReducao15ic,
+    mv.modelo_veiculo_reducao_16ic  AS modeloVeiculoReducao16ic,
+    mv.modelo_veiculo_reducao_17ic  AS modeloVeiculoReducao17ic,
+    mv.modelo_veiculo_reducao_18ic  AS modeloVeiculoReducao18ic,
+    mv.modelo_veiculo_reducao_19ic  AS modeloVeiculoReducao19ic,
+    mv.modelo_veiculo_reducao_20ic  AS modeloVeiculoReducao20ic,
+
+    CASE 
+        WHEN COALESCE(mv.modelo_veiculo_reducao_1ic,  0) = 0 THEN 0
+        WHEN COALESCE(mv.modelo_veiculo_reducao_2ic,  0) = 0 THEN 1
+        WHEN COALESCE(mv.modelo_veiculo_reducao_3ic,  0) = 0 THEN 2
+        WHEN COALESCE(mv.modelo_veiculo_reducao_4ic,  0) = 0 THEN 3
+        WHEN COALESCE(mv.modelo_veiculo_reducao_5ic,  0) = 0 THEN 4
+        WHEN COALESCE(mv.modelo_veiculo_reducao_6ic,  0) = 0 THEN 5
+        WHEN COALESCE(mv.modelo_veiculo_reducao_7ic,  0) = 0 THEN 6
+        WHEN COALESCE(mv.modelo_veiculo_reducao_8ic,  0) = 0 THEN 7
+        WHEN COALESCE(mv.modelo_veiculo_reducao_9ic,  0) = 0 THEN 8
+        WHEN COALESCE(mv.modelo_veiculo_reducao_10ic, 0) = 0 THEN 9
+        WHEN COALESCE(mv.modelo_veiculo_reducao_11ic, 0) = 0 THEN 10
+        WHEN COALESCE(mv.modelo_veiculo_reducao_12ic, 0) = 0 THEN 11
+        WHEN COALESCE(mv.modelo_veiculo_reducao_13ic, 0) = 0 THEN 12
+        WHEN COALESCE(mv.modelo_veiculo_reducao_14ic, 0) = 0 THEN 13
+        WHEN COALESCE(mv.modelo_veiculo_reducao_15ic, 0) = 0 THEN 14
+        WHEN COALESCE(mv.modelo_veiculo_reducao_16ic, 0) = 0 THEN 15
+        WHEN COALESCE(mv.modelo_veiculo_reducao_17ic, 0) = 0 THEN 16
+        WHEN COALESCE(mv.modelo_veiculo_reducao_18ic, 0) = 0 THEN 17
+        WHEN COALESCE(mv.modelo_veiculo_reducao_19ic, 0) = 0 THEN 18
+        WHEN COALESCE(mv.modelo_veiculo_reducao_20ic, 0) = 0 THEN 19
+        ELSE 0
+    END AS qtdMarchaa,
+
+    mv.modelo_veiculo_coef_atrito AS coeficienteAtrito,
+    mv.modelo_veiculo_raio_dinam  AS raio,
+    mv.modelo_veiculo_resist_rolam AS resistenciaRolamento,
+    mv.modelo_veiculo_tipo_pneu   AS tipoPneu,
+    mv.modelo_veiculo_torque_max  AS torque,
+    mv.modelo_veiculo_reducao_max AS reducaoMax
+
+FROM aet04.projeto p
+JOIN aet04.ordem_servico_servico oss ON oss.ordem_servico_servico_id = p.ordem_servico_servico_id 
+JOIN aet04.veiculo v               ON oss.veiculo_id = v.veiculo_id 
+JOIN aet04.modelo_veiculo mv       ON mv.modelo_veiculo_id = v.modelo_veiculo_id 
+JOIN aet04.tipo_servico ts         ON ts.tipo_servico_id = oss.tipo_servico_id  
+JOIN aet04.tipo_veiculo tv         ON tv.tipo_veiculo_id  = ts.tipo_veiculo_id 
+JOIN aet04.tipo_veiculo_eixo tve   ON tve.tipo_veiculo_id = tv.tipo_veiculo_id 
+                                   AND tve.tipo_veiculoEixo_numeixos = p.projeto_num_eixos 
+                                   AND tve.modelo_veiculo_id = mv.modelo_veiculo_id
+JOIN aet04.tve_nivel tn            ON tn.tipo_veiculoEixo_id = tve.tipo_veiculoEixo_id
+JOIN aet04.tve_niveltipoeixo tn1   ON tn1.TVE_Nivel_id = tn.TVE_Nivel_id
+WHERE p.projeto_id = :projetoId 
+  AND tv.tipo_veiculo_ativo = 1
+  AND (
+        SELECT SUM(a.TipoEixo_distancia)
+        FROM aet04.tve_nivel b
+        JOIN aet04.tve_niveltipoeixo a ON a.TVE_Nivel_id = b.TVE_Nivel_id
+        WHERE b.tipo_veiculoEixo_id = tve.tipo_veiculoEixo_id
+      ) = p.projeto_comprimento
+""", nativeQuery = true)
+    ProjetoResumoEixosDTO buscarResumoPorProjeto(@Param("projetoId") Long projetoId);
+
 
     @Query(value = """
-            SELECT p.projeto_id,
-            case tipo_distancia_entre_eixos
-            when 1 then 'A'
-            when 2 then 'B'
-            when 3 then 'C'
-            when 4 then 'D'
-            when 5 then 'E'
-            when 6 then 'F'
-            when 7 then 'G'
-            when 8 then 'H'
-            when 9 then 'I'
-            when 10 then 'J'
-            when 11 then 'K'
-            when 12 then 'L'
-            when 13 then 'M'
-            when 14 then 'N'
-            when 15 then 'O'
-            when 16 then 'P'
-            end AS LETRA,
-            tn1.TipoEixo_rodas ,
-            case tn1.TipoEixo_tandem
-            when 1 then 'Sim' else 'Não' end as TipoEixo_tandem,
-            tn1.TipoEixo_distancia,
-            tn1.TipoEixo_tipo ,
-            case tn1.TipoEixo_tipo
-            when 1 then 'Eixo Simples'
-            when 2 then 'Eixo Duplo'
-            when 3 then 'Eixo Triplo'
-            when 4 then 'Balanço traseiro'
-            when 5 then 'Eixo Dianteiro'
-            else 'Distância entre eixo'
-            end as eixo,
-            TipoEixo_L,
-            case when TipoEixo_L > 10 then 2 else 1 end as fator_L,
-            tn1.TipoEixo_Peso ,
-            tn.TVE_Nivel_Nivel nivel
-            from aet04.projeto p
-            inner join aet04.ordem_servico_servico oss on oss.ordem_servico_servico_id = p.ordem_servico_servico_id
-            inner join aet04.veiculo v on oss.veiculo_id = v.veiculo_id
-            inner join aet04.modelo_veiculo mv on mv.modelo_veiculo_id = v.modelo_veiculo_id
-            inner join aet04.tipo_servico ts on ts.tipo_servico_id = oss.tipo_servico_id 
-            inner join aet04.tipo_veiculo tv on tv.tipo_veiculo_id  = ts.tipo_veiculo_id
-            inner join aet04.tipo_veiculo_eixo tve on tve.tipo_veiculo_id  = tv.tipo_veiculo_id
-            and tve.tipo_veiculoEixo_numeixos = p.projeto_num_eixos
-            and tve.modelo_veiculo_id = mv.modelo_veiculo_id
-            inner join aet04.tve_nivel tn on tn.tipo_veiculoEixo_id = tve.tipo_veiculoEixo_id
-            inner join aet04.tve_niveltipoeixo tn1 on tn1.TVE_Nivel_id = tn.TVE_Nivel_id
-            where p.projeto_id = :projetoId
-             and tv.tipo_veiculo_ativo = 1
-            and (select sum(a.TipoEixo_distancia ) from aet04.tve_nivel b
-            inner join aet04.tve_niveltipoeixo a on a.TVE_Nivel_id = b.TVE_Nivel_id
-            	where b.tipo_veiculoEixo_id = tve.tipo_veiculoEixo_id) = p.projeto_comprimento            
-            """, nativeQuery = true)
-    List<ProjetoEixosDetalheView> buscarDetalhesPorProjeto(Long projetoId);
+SELECT 
+    p.projeto_id AS projetoId,
+
+    CASE tipo_distancia_entre_eixos
+        WHEN 1  THEN 'A' WHEN 2  THEN 'B' WHEN 3  THEN 'C' WHEN 4  THEN 'D'
+        WHEN 5  THEN 'E' WHEN 6  THEN 'F' WHEN 7  THEN 'G' WHEN 8  THEN 'H'
+        WHEN 9  THEN 'I' WHEN 10 THEN 'J' WHEN 11 THEN 'K' WHEN 12 THEN 'L'
+        WHEN 13 THEN 'M' WHEN 14 THEN 'N' WHEN 15 THEN 'O' WHEN 16 THEN 'P'
+    END AS letra,
+
+    tn1.TipoEixo_rodas      AS tipoEixoRodas,
+    CASE tn1.TipoEixo_tandem WHEN 1 THEN 'Sim' ELSE 'Não' END AS tipoEixoTandem,
+    tn1.TipoEixo_distancia  AS tipoEixoDistancia,
+    tn1.TipoEixo_tipo       AS tipoEixoTipo,
+
+    CASE tn1.TipoEixo_tipo
+        WHEN 1 THEN 'Eixo Simples'
+        WHEN 2 THEN 'Eixo Duplo'
+        WHEN 3 THEN 'Eixo Triplo'
+        WHEN 4 THEN 'Balanço traseiro'
+        WHEN 5 THEN 'Eixo Dianteiro'
+        ELSE 'Distância entre eixo'
+    END AS eixo,
+
+    TipoEixo_L              AS tipoEixoL,
+    CASE WHEN TipoEixo_L > 10 THEN 2 ELSE 1 END AS fatorL,
+    tn1.TipoEixo_Peso       AS tipoEixoPeso,
+    tn.TVE_Nivel_Nivel      AS nivel
+
+FROM aet04.projeto p 
+JOIN aet04.ordem_servico_servico oss ON oss.ordem_servico_servico_id = p.ordem_servico_servico_id 
+JOIN aet04.veiculo v               ON oss.veiculo_id = v.veiculo_id 
+JOIN aet04.modelo_veiculo mv       ON mv.modelo_veiculo_id = v.modelo_veiculo_id 
+JOIN aet04.tipo_servico ts         ON ts.tipo_servico_id = oss.tipo_servico_id  
+JOIN aet04.tipo_veiculo tv         ON tv.tipo_veiculo_id  = ts.tipo_veiculo_id 
+JOIN aet04.tipo_veiculo_eixo tve   ON tve.tipo_veiculo_id  = tv.tipo_veiculo_id 
+                                   AND tve.tipo_veiculoEixo_numeixos = p.projeto_num_eixos 
+                                   AND tve.modelo_veiculo_id = mv.modelo_veiculo_id
+JOIN aet04.tve_nivel tn            ON tn.tipo_veiculoEixo_id = tve.tipo_veiculoEixo_id
+JOIN aet04.tve_niveltipoeixo tn1   ON tn1.TVE_Nivel_id = tn.TVE_Nivel_id
+WHERE p.projeto_id = :projetoId
+  AND tv.tipo_veiculo_ativo = 1
+  AND (
+        SELECT SUM(a.TipoEixo_distancia)
+        FROM aet04.tve_nivel b
+        JOIN aet04.tve_niveltipoeixo a ON a.TVE_Nivel_id = b.TVE_Nivel_id 
+        WHERE b.tipo_veiculoEixo_id = tve.tipo_veiculoEixo_id
+      ) = p.projeto_comprimento
+""", nativeQuery = true)
+    List<ProjetoEixosDetalheView> buscarDetalhesPorProjeto(@Param("projetoId") Long projetoId);
 
 }
